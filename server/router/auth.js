fd3c1670ac963743054129d9c6092186898ff652
Pravas Chandra Sarkar <106769339+PravasTheDeveloper@users.noreply.github.com>
2023-05-07 20:27:00 +0600

    login push

--- server/router/auth.js
+++ server/router/auth.js
@@ -4,52 +4,83 @@ const CryptoJS = require("crypto-js")
 
 router.post("/register", async (req, res) => {
 
-    const {username , email , upassword , cpassword} = req.body;
+    const { username, email, upassword, cpassword } = req.body;
 
-    if(!username || !email || !upassword || !cpassword){
-        res.status(422).json({error : "Please Fill All The Feild"})
-    }else{
-        try{
-            const UserExist = await User.findOne({email})
+    if (!username || !email || !upassword || !cpassword) {
+        res.status(422).json({ error: "Please Fill All The Feild" })
+    } else {
+        try {
+            const UserExist = await User.findOne({ email })
 
-            if(UserExist){
-                res.status(423).json({error:"This Email Is Already Taken"});
-            }else{
-                if(upassword === cpassword){
+            if (UserExist) {
+                res.status(423).json({ error: "This Email Is Already Taken" });
+            } else {
+                if (upassword === cpassword) {
                     const password = CryptoJS.AES.encrypt(upassword, process.env.PASS_SEC);
                     const registerUser = new User({
-                        username , email , password
+                        username, email, password
                     });
 
                     const registered = await registerUser.save()
 
-                    if(registered){
-                        res.status(400).json({messege : "Detail Saved"})
-                    }else{
-                        res.status(401).json({error : "Your Password Is Not Same ! "})
+                    if (registered) {
+                        res.status(400).json({ messege: "Detail Saved" })
+                    } else {
+                        res.status(401).json({ error: "Your Password Is Not Same ! " })
                     }
                 }
             }
-        }catch(err){
-            res.status(400).json({error: `${err}`})
+        } catch (err) {
+            res.status(400).json({ error: `${err}` })
         }
     }
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400).json({ error: "Plese Fill All The Fill" });
+    } else {
+        try {
+            const mainUser = await AllUser.findOne({ email: email });
+
+            if (mainUser) {
+                const hashedPassword = CryptoJS.AES.decrypt(
+                    password,
+                    process.env.PASS_SEC
+                );
 
 
+                const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+                if (originalPassword === password) {
+                    const accessToken = jwt.sign(
+                        {
+                            id: mainUser._id,
+                            isAdmin: mainUser.isAdmin,
+                        },
+                        process.env.JWT_SEC,
+                        { expiresIn: "3d" }
+                    );
+
+                    const { password, ...others } = user._doc;
+                    res.status(200).json({ ...others, accessToken });
+                    res.send("LOGIN SUCCESSFULL");
 
 
-    // const newUser = new User({
-    //     username: req.body.username,
-    //     email: req.body.email,
-    //     password: req.body.password,
-    // });
+                } else {
+                    res.status(400).json({ error: "Invalid Login !" });
+                }
+            } else {
+                res.status(400).json({ error: "Invalid Login !" });
+            }
 
-    // try {
-    //     const savedUser = await newUser.save();
-    //     res.status(201).json(savedUser);
-    // } catch (err) {
-    //     res.status(500).json(err);
-    // }
-})
+
+        } catch (error) {
+            res.status(400).json({ error: "Something Went Wrong" });
+            console.log(error);
+        }
+    }
+});
 
 module.exports = router
