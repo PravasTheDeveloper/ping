f80260f1cef0dac6b2db192d6364b237ecddf795
Pravas Chandra Sarkar <106769339+PravasTheDeveloper@users.noreply.github.com>
2023-05-27 16:27:34 +0600

    day 01-04(02)

--- src/components/HomePage/NewProduct.jsx
+++ src/components/HomePage/NewProduct.jsx
@@ -4,8 +4,10 @@ import ProductPage from './ProductPage'
 function NewProduct() {
     return (
         <>
-            <div className='w-full h-full bg-white container mx-auto mt-10 md:p-20 pt-10 rounded-lg'>
-                <ProductPage name="New Product" />
+            <div className='w-full h-full px-5'>
+                <div className='w-full h-full bg-white container mx-auto mt-10 md:p-20 pt-10 rounded-lg'>
+                    <ProductPage name="New Product" />
+                </div>
             </div>
         </>
     )
